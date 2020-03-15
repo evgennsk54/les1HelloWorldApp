a3915adb2f7519eeed0c4da929ee4c5f6e14a0bc
Evgen <Evgennsk54@gmail.com>
2020-03-15 12:55:23 +0700

    Initial Commit

---
+++ les1HelloWorldApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  les1HelloWorldApp
+//
+//  Created by Евгений Каратаев on 15.03.2020.
+//  Copyright © 2020 Evgen Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
